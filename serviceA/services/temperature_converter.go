2a3cb6d24effc27acab267cd0431f32d4cec1cd3
nrodrigues <nrodrigues@ionic.health>
2024-06-10 21:03:01 -0300

    ajustes

--- serviceA/services/temperature_converter.go
+++
@@ -1,9 +0,0 @@
-package services
-
-func CelsiusToFahrenheit(celsius float64) float64 {
-	return (celsius * 1.8) + 32
-}
-
-func CelsiusToKelvin(celsius float64) float64 {
-	return celsius + 273.15
-}
